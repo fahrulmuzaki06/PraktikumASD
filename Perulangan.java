3bc6eae32659dba13f2244e6e326792f71437394
fahrulmuzaki <fahrulmuzaki06@gmail.com>
2026-02-14 14:47:22 +0700

    Percobaan 2

---
+++ Perulangan.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Perulangan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("NIM : ");
+        String nim = sc.next();
+        
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.println("n : " + n);
+        System.out.print("");
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 10 || i == 15) {
+                continue;
+            }
+            if (i % 3 == 0){
+                System.out.print("# ");
+            }
+            else if (i % 2 ==0){
+                System.out.print(i + " ");
+            }
+            else {
+                System.out.print("* ");
+            }
+        }
+        sc.close();
+    }   
+}
