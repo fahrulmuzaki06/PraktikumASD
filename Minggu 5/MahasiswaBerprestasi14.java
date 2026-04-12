1ced544931ac03ca71bcfddec4e0b3e7ee951ea0
fahrulmuzaki <fahrulmuzaki06@gmail.com>
2026-04-12 19:10:49 +0700

    Praktikum 2

---
+++ Minggu 5/MahasiswaBerprestasi14.java
@@ -0,0 +1,33 @@
+public class MahasiswaBerprestasi14 {
+    Mmahasiswa14 [] listMhs = new Mmahasiswa14 [5];
+    int idx;
+    void tambah(Mmahasiswa14 m){
+        if (idx<listMhs.length){
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+
+        }
+    }
+
+    void tampil(){
+        for (Mmahasiswa14 m : listMhs){
+            m.tampilinformasi();
+            System.out.println("-------------------");
+        }
+    }
+
+    void bubbleSort(){
+        for (int i=0; i<listMhs.length-1; i++){
+            for (int j=1; j<listMhs.length-i; j++){
+                if (listMhs[j].ipk > listMhs[j - 1].ipk){
+                    Mmahasiswa14 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
+                }
+            }
+        }
+    }
+    
+}
---
+++ Minggu 5/MahasiswaDemo14.java
@@ -0,0 +1,26 @@
+public class MahasiswaDemo14 {
+    public static void main(String[] args) {
+        MahasiswaBerprestasi14 list = new MahasiswaBerprestasi14();
+        Mmahasiswa14 m1 = new Mmahasiswa14("123", "Jidan", "2A", 3.2);
+        Mmahasiswa14 m2 = new Mmahasiswa14("124", "Ayu", "2A", 3.5);
+        Mmahasiswa14 m3 = new Mmahasiswa14("125", "Sofi", "2A", 3.1);
+        Mmahasiswa14 m4 = new Mmahasiswa14("126", "Sita", "2A", 3.9);
+        Mmahasiswa14 m5 = new Mmahasiswa14("127", "Miki", "2A", 3.7);
+
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
+
+        System.out.println("Data mahasiswa sebelum sorting: ");
+        list.tampil();
+
+        System.out.println("Dta mahasiswa setelah sorting berdasarkan ipk (DESC) : ");
+        list.bubbleSort();
+        list.tampil();
+
+
+    }
+    
+}
