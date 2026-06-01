20f2ccd93cb30c68940da75f96a3090fdb1b623c
fahrulmuzaki06 <fahrulmuzaki06@gmail.com>
2026-06-01 16:02:31 +0700

    Percobaan 2

--- Minggu 9/StackTugasMahasiswa.java
+++ Minggu 9/StackTugasMahasiswa.java
@@ -68,4 +68,19 @@ public class StackTugasMahasiswa {
     public int getJumlahTugas() {
     return top + 1;
     }
+
+    public String konversiDesimalKebiner(int nilai){
+            StackKonversi14 stack = new StackKonversi14(32);
+            while (nilai > 0){
+                int sisa = nilai % 2;
+                stack.push(sisa);
+                nilai = nilai / 2;
+
+            }
+            String biner = new String();
+            while (!stack.isEmpty()){
+                biner +=  stack.pop();
+            }
+            return biner;
+        }
 }
